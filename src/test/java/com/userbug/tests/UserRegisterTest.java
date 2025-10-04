af393dd2328ac035824cd648ec524eabef863174
Arseniy Mikhailov <miksenya8@gmail.com>
2025-10-04 22:56:09 +0500

    Перенес вообще все что можно из тестов На все действия создал пошаговые сценарии - пакет Scenarios В support: DataGen - генерирует уникальный мэил CompanySteps - шаги для регистрации компании, что бы, например, тест не падал при регистрации нового пользователя

--- src/test/java/com/userbug/tests/UserRegisterTest.java
+++ src/test/java/com/userbug/tests/UserRegisterTest.java
@@ -1,32 +1,13 @@
 package com.userbug.tests;
 
 import com.userbug.tests.base.BaseApiTest;
-import pageuserbug.client.UserClient;
-import pageuserbug.model.request.DoRegisterRequest;
-import io.restassured.response.Response;
+import pageuserbug.scenarios.RegisterUserScenario;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserRegisterTest extends BaseApiTest {
 
-    private final UserClient userClient = new UserClient();
-
     @Test
-    void doRegisterTest() {
-        DoRegisterRequest req = new DoRegisterRequest(
-                "4ewfdwdf@bk.ru",
-                "Roman3",
-                "2"
-        );
-
-        Response response = userClient.doRegister(req)
-                .then()
-                .log().all()
-                .statusCode(200)
-                .extract().response();
-
-        String name = response.jsonPath().getString("name");
-        assertEquals("Roman3", name, "Имена не совпадают");
+    void doRegister_minimal() {
+        new RegisterUserScenario().run(); // всё создаёт и проверяет внутри
     }
 }
